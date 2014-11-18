0f0aa4ef861211f957321fb82c184f71d3be0d82
Brian Kennedy <briankennedy1@gmail.com>
2014-11-17 20:20:23 -0800

    Getting data in

--- footballnames.js
+++ footballnames.js
@@ -1,25 +1,24 @@
+PlayerNameDB = new Meteor.Collection('playerNameDB');
+
 
 Router.route('/', function () {
-  this.render('homePage');
+  this.render('HomePage');
+});
+
+Router.route('/player', function(){
+  this.render('PlayerDetails')
 });
 
 if (Meteor.isClient) {
 
-  // counter starts at 0
-  Session.setDefault("counter", 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
+  Template.HomePage.helpers({
+  
+    player : function(){
+      return PlayerNameDB.find({});
     }
+
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-    }
-  });
 }
 
 if (Meteor.isServer) {
