c63d97ecf2a06f8b81aeaee9a2904cad00d0a978
Brian Kennedy <briankennedy1@gmail.com>
2014-11-17 21:34:26 -0800

    Pretty far, got routing to work

--- footballnames.js
+++ footballnames.js
@@ -5,13 +5,25 @@ Router.route('/', function () {
   this.render('HomePage');
 });
 
-Router.route('/player', function(){
-  this.render('PlayerDetails')
+Router.route('/player/:_id', {
+  template: 'PlayerDetails',
+  name: 'PlayerDetails',
+  data: function () {
+    return PlayerNameDB.findOne({_id: this.params._id});
+  }
 });
+
 
 if (Meteor.isClient) {
 
   Template.HomePage.helpers({
+  
+    player : function(){
+      return PlayerNameDB.find({});
+    }
+
+  });
+  Template.PlayerDetails.helpers({
   
     player : function(){
       return PlayerNameDB.find({});
