14eb5c9e94ad7ec1429ab5853af1cee9f7d30be9
Brian Kennedy <briankennedy1@gmail.com>
2014-11-21 07:40:46 -0800

    Jotting down ideas

--- footballnames.js
+++ footballnames.js
@@ -1,5 +1,16 @@
 PlayerNameDB = new Meteor.Collection('playerNameDB');
+/*
+  Thinking about making a couple new meteor collections.
+  One would be team names so I can just pull them from a collection instead
+    of dealing with 1500 results with team names repeated.
+  The other would be a "tags" collection where tags like "feminine" and "cowboy"
+    would be stored and INCREMENTED every time someone adds a tag. That way I can
+    pull from that DB and say "15 people have the cowboy" tag, but link to the result
+    by pulling from PlayerNameDB and pull the specific players: 
+    "PlayerNameDB.find({}, { sort: {tags: 1}, fields: {tags: true} });"
 
+
+*/
 
 Router.route('/', function () {
   this.render('HomePage');
