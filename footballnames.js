8f431394f4637fee2e641bd7ef33b22ed8fef3fe
Brian Kennedy <briankennedy1@gmail.com>
2014-11-24 18:27:08 -0800

    Offloaded teams to TeamNameDB
    Tried keyword routing but didn't make any ground

--- footballnames.js
+++ footballnames.js
@@ -1,16 +1,13 @@
 PlayerNameDB = new Meteor.Collection('playerNameDB');
 PlayerTags = new Meteor.Collection('playerTagDB');
-
+TeamNameDB = new Meteor.Collection('teamNameDB');
 /*
-  Thinking about making a couple new meteor collections.
-  One would be team names so I can just pull them from a collection instead
-    of dealing with 1500 results with team names repeated.
-  The other would be a "tags" collection where tags like "feminine" and "cowboy"
+    A "tags" collection where tags like "feminine" and "cowboy"
     would be stored and INCREMENTED every time someone adds a tag. That way I can
     pull from that DB and say "15 people have the cowboy" tag, but link to the result
     by pulling from PlayerNameDB and pull the specific players: 
-    "PlayerNameDB.find({}, { sort: {tags: 1}, fields: {tags: true} });"
 
+    PlayerNameDB.find({}, { sort: {team: 1}, fields: {team: true} });
 
 */
 
@@ -27,13 +24,12 @@ Router.route('/:_id/:firstName:lastName:jerseyNumber/', {
 });
 
 
-
 if (Meteor.isClient) {
 
   Template.HomePage.helpers({
     teamList : function(){
-     var teamList = PlayerNameDB.find({}, { sort: {team: 1}, fields: {team: true} });
-     return teamList;
+      teamList = TeamNameDB.find({}, {});
+      return teamList;
     },
     player : function(){
       var teamName = Session.get("teamName");
@@ -48,17 +44,12 @@ if (Meteor.isClient) {
   
     player : function(){
       return PlayerNameDB.find({});
-    },
-    tag : function(){
-      return PlayerNameDB.find({_id: this._id});
     }
-
 
   });
   Template.HomePage.events({
     'change #teamSelect' : function(event, template){
       var teamName = template.find('#teamSelect').value;
-      console.log(teamName)
       return Session.set("teamName", teamName);
     }
 
